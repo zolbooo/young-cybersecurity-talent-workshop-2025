41af963ff5c88c0e5be103b7636d5f824d8533fe
Zolboobayar Gantumur <hello@zolbooo.xyz>
2025-11-26 15:04:48 +0800

    fix(start-wazuh): override only API and dashboard password

--- services/start-wazuh.sh
+++ services/start-wazuh.sh
@@ -5,17 +5,13 @@ if [ ! -d ./config/wazuh_indexer_ssl_certs ]; then
 	docker compose -f generate-indexer-certs.yml run --rm generator
 fi
 
-if grep SecretPassword docker-compose.yml > /dev/null; then
-	INDEXER_PASSWORD=$(openssl rand -base64 16 | tr -d '/=+')
-	sed -i "s/SecretPassword/$INDEXER_PASSWORD/g" docker-compose.yml
-fi
 if grep 'MyS3cr37P450r.*-' docker-compose.yml > /dev/null; then
-	API_PASSWORD=$(openssl rand -base64 16 | tr -d '/=+')
+	API_PASSWORD=$(openssl rand -base64 32 | tr '/=+' '*@#')
 	sed -i "s/MyS3cr37P450r.*/$API_PASSWORD/g" docker-compose.yml
 fi
 sed -i "s/DASHBOARD_USERNAME=kibanaserver/DASHBOARD_USERNAME=admin/g" docker-compose.yml
 if grep 'DASHBOARD_PASSWORD=kibanaserver' docker-compose.yml > /dev/null; then
-	DASHBOARD_PASSWORD=$(openssl rand -base64 16 | tr -d '/=+')
+	DASHBOARD_PASSWORD=$(openssl rand -base64 32 | tr '/=+' '*@#')
 	echo "Admin credentials: admin:$DASHBOARD_PASSWORD"
 	sed -i "s/DASHBOARD_PASSWORD=kibanaserver/DASHBOARD_PASSWORD=$DASHBOARD_PASSWORD/g" docker-compose.yml
 fi
