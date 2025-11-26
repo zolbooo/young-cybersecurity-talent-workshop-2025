177e88c553342712ebd8d4d3829de1765a7ec3a1
Zolboobayar Gantumur <hello@zolbooo.xyz>
2025-11-26 15:08:43 +0800

    fix(start-wazuh): don't override any passwords

--- services/start-wazuh.sh
+++ services/start-wazuh.sh
@@ -5,15 +5,4 @@ if [ ! -d ./config/wazuh_indexer_ssl_certs ]; then
 	docker compose -f generate-indexer-certs.yml run --rm generator
 fi
 
-if grep 'MyS3cr37P450r.*-' docker-compose.yml > /dev/null; then
-	API_PASSWORD=$(openssl rand -base64 32 | tr '/=+' '*@#')
-	sed -i "s/MyS3cr37P450r.*/$API_PASSWORD/g" docker-compose.yml
-fi
-sed -i "s/DASHBOARD_USERNAME=kibanaserver/DASHBOARD_USERNAME=admin/g" docker-compose.yml
-if grep 'DASHBOARD_PASSWORD=kibanaserver' docker-compose.yml > /dev/null; then
-	DASHBOARD_PASSWORD=$(openssl rand -base64 32 | tr '/=+' '*@#')
-	echo "Admin credentials: admin:$DASHBOARD_PASSWORD"
-	sed -i "s/DASHBOARD_PASSWORD=kibanaserver/DASHBOARD_PASSWORD=$DASHBOARD_PASSWORD/g" docker-compose.yml
-fi
-
 docker compose up -d
