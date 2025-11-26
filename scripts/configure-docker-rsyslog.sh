6ac92f4ed8c08760585fb3addc4617df21989eb6
Zolboobayar Gantumur <hello@zolbooo.xyz>
2025-11-26 16:14:56 +0800

    feat: configure Docker with rsyslog

--- scripts/configure-docker-rsyslog.sh
+++ scripts/configure-docker-rsyslog.sh
@@ -23,3 +23,27 @@ else
     | sponge /etc/docker/daemon.json
 fi
 systemctl restart docker
+
+if [ ! -f /etc/rsyslog.d/wazuh-docker.conf ]; then
+    cat << EOF > /etc/rsyslog.d/wazuh-docker.conf
+$FileCreateMode 0644
+$template DockerDaemonLogFileName,"/var/log/docker/docker.log"
+$template DockerContainerLogFileName,"/var/log/docker/%SYSLOGTAG:R,ERE,1,FIELD:docker/(.*)\[--end:secpath-replace%.log"
+if $programname == 'dockerd' then {
+  ?DockerDaemonLogFileName
+  stop
+}
+if $programname == 'containerd' then {
+  ?DockerDaemonLogFileName
+  stop
+}
+if $programname == 'docker' then {
+  if $syslogtag contains 'docker/' then {
+  ?DockerContainerLogFileName
+  stop
+  }
+}
+$FileCreateMode 0600
+EOF
+fi
+systemctl restart rsyslog
