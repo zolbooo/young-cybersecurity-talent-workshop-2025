89b72ea0e0f2b66307541f6204a45ea367781ff9
Zolboobayar Gantumur <hello@zolbooo.xyz>
2025-11-26 16:09:11 +0800

    fix(docker-rsyslog): require script to be run as root

--- scripts/configure-docker-rsyslog.sh
+++ scripts/configure-docker-rsyslog.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
+if whoami | grep -qv root; then
+  echo "This script must be run as root."
+  exit 1
+fi
 
 if [ ! -f /etc/docker/daemon.json ]; then
-    sudo touch /etc/docker/daemon.json
-    cat << EOF | sudo tee /etc/docker/daemon.json
+    cat << EOF > /etc/docker/daemon.json
 {
   "log-driver": "syslog",
   "log-opts": {
@@ -13,7 +16,7 @@ if [ ! -f /etc/docker/daemon.json ]; then
 EOF
 else
   # Note: sponge is part of moreutils package
-  sudo jq '. + {"log-driver":"syslog","log-opts":{"syslog-address":"unixgram:///dev/log","tag":"docker/{{.Name}}"}}' /etc/docker/daemon.json \
-    | sudo sponge /etc/docker/daemon.json
+  jq '. + {"log-driver":"syslog","log-opts":{"syslog-address":"unixgram:///dev/log","tag":"docker/{{.Name}}"}}' /etc/docker/daemon.json \
+    | sponge /etc/docker/daemon.json
 fi
-sudo systemctl restart docker
+systemctl restart docker
